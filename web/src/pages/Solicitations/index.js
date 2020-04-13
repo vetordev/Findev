efce54c7b891322267e794662a7d0c145c2bbedd
unknown <heyvitoria.lopes@gmail.com>
2020-04-13 19:13:54 -0300

    sessões de enviadas e recebidas na tela solicitations

--- web/src/pages/Solicitations/index.js
+++ web/src/pages/Solicitations/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 import SolicitationCard from '../../components/SolicitationCard';
+import { Content } from '../../components/Content';
 
-import { Container, Content } from './styles';
+import { Container, Sessions } from './styles';
 
 export default function Solicitations() {
+  const [sent, setSent] = useState('active');
+  const [received, setReceived] = useState('disabled');
+
+  function handleActive() {
+    setSent(sent === 'active' ? 'disabled' : 'active');
+    setReceived(received === 'active' ? 'disabled' : 'active');
+  }
+
   return (
     <>
       <Header page="solicitations" />
       <Container>
-        <Content>
+        <Sessions>
+          <div id={sent} onClick={handleActive}>Enviados</div>
+          <div id={received} onClick={handleActive}>Recebidos</div>
+        </Sessions>
+        <Content width="90%">
         </Content>    
       </Container>
     </>
--- web/src/pages/Solicitations/styles.js
+++ web/src/pages/Solicitations/styles.js
@@ -1,7 +1,48 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     flex-direction: column;
+     padding: 0 150px 50px 150px;
+
+     @media (max-width: 718px) {
+          padding: 0;
+     }
 `;
 
-export const Content = styled.div``;
+export const Sessions = styled.div`
+     width: 100%;
+     /* padding: 0 60px; */
+
+     display: flex;
+     justify-content: center;
+     align-items: center;
+
+     div {
+          cursor: pointer;
+          height: 50px;
+          width: 30%;
+          border-radius: 12px 12px 0 0;
+          margin: 0 50px;
+
+          font-weight: bold;
+          font-size: 20px;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+     }
+
+     div#active {
+          background: #000;
+          /* margin-right: 100px; */
+          color: #fff;
+     }
+
+     div#disabled {
+          background: rgba(0, 0, 0, 0.2);
+          color: rgba(255, 255, 255, 0.6);
+     }
+`;
